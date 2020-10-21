c406df5f982e9eb3c7d6c2f4c33fd87468e77c6f
Ruth Ironside <ruthironside@live.com>
2020-10-21 11:45:10 +0100

    checked customer had cash

--- tests/customer_test.py
+++ tests/customer_test.py
@@ -6,3 +6,10 @@ class TestCustomer(unittest.TestCase):
 
     def setUp(self):
         self.customer = Customer("Michael Sinclair", 1000.00)
+
+    def test_does_customer_have_cash(self):
+        self.assertEqual(1000.00, self.customer.wallet)
+    
+    # def test_can_customer_buy_drink(self):
+
+    #     self.assertEqual()
